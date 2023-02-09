d737c0dddeeb61930e0aed57f341ee7ab32dc2b0
1Vidhyasagar <vidhyasagarmyana9598.com>
2023-02-09 14:16:42 +0530

    project section modified

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,24 +8,32 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
+      abc: "A user-friendly website showcasing a comprehensive list of breweries from API with detailed information.",
+      title: "Brewery List",
       src: brewery,
       href1: "https://breweries-shop.netlify.app/",
       href2: "https://github.com/1Vidhyasagar/shopbeer",
     },
     {
       id: 2,
+      abc: "A website displaying Covid-19 case reports of various countries, created to provide real-time information.",
+      title: "Covid Tracker",
       src: covid,
       href1: "https://covidtracker-world.netlify.app/",
       href2: "https://github.com/1Vidhyasagar/Day_25.-3-",
     },
     {
       id: 3,
+      abc: "Tic Tac Toe game, play against computer on a static web app, easy-to-use, designed for fun and enjoyment!",
+      title: "Fun game",
       src: game,
       href1: "https://gametictactoe1.netlify.app/",
       href2: "https://github.com/1Vidhyasagar/game",
     },
     {
       id: 4,
+      abc: "A website displaying a comprehensive list of movies with detailed information, including ratings and reviews.",
+      title: "Box office",
       src: movies,
       href1: "https://movies-1list.netlify.app/",
       href2: "https://github.com/1Vidhyasagar/Day_25.-2-",
@@ -36,25 +44,31 @@ const Projects = () => {
       name="projects"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full pt-20">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full pt-28">
         <div>
           <p className="text-2xl font-bold">Projects</p>
-          <p className="pt-1 ">Here are my completed projects</p>
         </div>
-        <div className="pt-2 grid sm:grid-cols-2 md:grid-cols-4 gap-8 px-12 sm:px-0 text-sm">
-          {projects.map(({ id, src, href1, href2 }) => (
+
+        <div
+          className="pt-1 grid sm:grid-cols-2 md:grid-cols-4 gap-8 mr-10 sm:px-0 text-sm"
+          style={{ width: "1000px" }}
+        >
+          {projects.map(({ id, title, abc, src, href1, href2 }) => (
             <div key={id} className="shadow-md shadow-gray-500 rounded-lg">
+              <p className="py-1 bg-black text-white text-sm  text-center w-full ">
+                Project : {title}
+              </p>
+              <div>
+                <p className=" text-center  text-xs bg-gray max-w-sm w-full text-gray-400">
+                  {abc}
+                </p>
+              </div>
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105 px-1"
               />
-              <div className="text-1xl flex items-center justify-center">
-                <p>
-                  Project title <br />
-                  description
-                </p>
-              </div>
+            
               <div
                 className="flex items-center justify-center
                  bg-black"
@@ -95,7 +109,7 @@ const Projects = () => {
           ))}
         </div>
 
-        <div className="mt-8">
+        <div className="mt-6">
           <p>Currently I am working on following projects</p>
           <ul className="list-disc text-sm ">
             <li>Movie ticket booking app/site</li>
