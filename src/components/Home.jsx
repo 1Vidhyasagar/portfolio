0ab35dfdce03576178f9cfecd43f82e27b403deb
1Vidhyasagar <vidhyasagarmyana9598.com>
2023-04-02 17:02:20 +0530

    screen sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,12 +14,12 @@ const Home = () => {
       name="home"
       className=" place-content-center h-screen w-full px-5 bg-gray-800 md:bg-gradient-to-bl from-gray-300  via-gray-900 to-black text-white text-left"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col  items-center justify-center h-full px-4 pl-8  md:flex-row overflow-hidden">
-        <div className="flex mt-4 flex-col justify-center h-full">
-          <h3 className=" lg:mt-0 md:mt-0 font-bold  sm:text-2xl   text-white ">
+      <div className="max-w-screen-lg mx-auto flex flex-col  items-center justify-center h-full px-4 pl-8 md:flex-row overflow-hidden">
+        <div className="flex mt-4 flex-col justify-center  h-full">
+          <h3 className=" lg:mt-0 md:-mt-96 font-bold  sm:text-2xl mt-10 text-white ">
             Hi' &nbsp; There <br />I am a&nbsp; full Stack Developer
           </h3>
-          <p className="pl-4 mt-2 mb-4 font-medium  text-gray-300 py-4 max-w-md">
+          <p className="pl-4  mb-2 font-medium  text-gray-300 py-4 max-w-md">
             Enthusiastic and adaptive in nature to work with MERN technology for
             developing web applications.
           </p>
@@ -31,7 +31,7 @@ const Home = () => {
               rel="noreferrer"
             >
               <button
-                className="bg-cyan-500 shadow-2xl shadow-cyan-900  text-white w-5/12 px-3 py-1 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-500  to-gray-900 cursor-pointer 
+                className="bg-cyan-500 shadow-2xl shadow-cyan-900  text-white w-5/12 px-3 py-1 flex items-center rounded-md bg-gradient-to-l from-cyan-500  to-gray-900 cursor-pointer 
               "
               >
                 <span className="hover:translate-x-6 duration-700 lg:ml-7">
@@ -43,12 +43,11 @@ const Home = () => {
             </a>
           </div>
         </div>
-        <div className=" shadow-2xl shadow-cyan-900 mx-auto bottom-0 md:h-4/6  lg:mt-48   w-5/12 rounded-full md:bg-gradient-to-bl to-cyan-700  via-gray-800 from-gray-900">
+        <div className="lg:rounded-full bottom-0 shadow-xl shadow-cyan-500 w-7/12 md:h-5/6 md:w-full bg-gradient-to-bl to-cyan-700  via-gray-800 from-gray-900 md:mt-auto lg:mb-5 lg:h-4/6 lg:w-3/6 xl:h-3/6 xl:w-3/6 lg:ml-auto">
           <img
             src={MyPhoto}
             alt="my profile"
-            className="  lg:-mt-20 -mt-5   right-0 lg:left-0 mx-auto md:h-6/6 md:w-64      
-        "
+            className=" md:h-full  md:mt-52 md:ml-20 lg:h-screen lg:-mt-16    right-0"
           />
         </div>
       </div>
